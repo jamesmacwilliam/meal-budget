1524d60c352f3f89093a092b7e3a6ffce880b24b
James Mac William <jimmy.macwilliam@gmail.com>
2018-02-07 00:10:25 -0500

    add ingredients queries

---
+++ src/server/db/queries/ingredients.js
@@ -0,0 +1,19 @@
+const conn = require('../connection')
+
+export function findAllByName(name) {
+  return conn('ingredients').
+    where('name', 'like' `%${name}%`).
+    select('*')
+}
+
+export function find(id) {
+  return conn('ingredients').
+    where({ id: parseInt(id) })
+    select('*')
+}
+
+export function createByName(name) {
+  return conn('ingredients').
+    insert({ name: name }).
+    returning('*')
+}
