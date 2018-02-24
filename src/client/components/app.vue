937aab8166c2ad38b9d51c3b01dc6d8d09a13ad9
James Mac William <jimmy.macwilliam@gmail.com>
2018-02-24 15:33:22 -0500

    Add page container + ensure vuetify css works

--- src/client/components/app.vue
+++ src/client/components/app.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+  <v-app id="mb-container">
+    <mb-navigation/>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+          >
+          <router-view/>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2018</span>
+    </v-footer>
+  </v-app>
 </template>
 <script>
+import MbNavigation from './mb-navigation.vue'
+
 export default {
+  components: { MbNavigation }
 }
 </script>
---
+++ src/client/components/mb-navigation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="navigation">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <li v-for="item in leftNavItems">
+          <router-link :to="item.path" class="list__tile" exact>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </router-link>
+        </li>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Meal Budget</v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      leftNavItems: [
+        {
+          icon: 'home',
+          title: 'Home',
+          path: '/'
+        }
+      ]
+    }
+  }
+}
+</script>
